7cb779b7f7c8f99b57765a3b96c1d40a4ffef53a
dferree <dan.ferree94@gmail.com>
2024-01-02 14:32:58 +0000

    base app

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+// pascalCasing, capitalise first letter of each word
+function Message() {
+    // JSX: Javascript XML
+    // JSZX allows us to create dynamic
+    const name = "Daniel"
+    if (name)
+        return <h1> Hello {name}</h1>;
+    return <h1>Hello World</h1>;
+}
+
+export default Message
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,13 @@
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item"> An item</li>
+      <li className="list-group-item"> An item</li>
+      <li className="list-group-item"> An item</li>
+      <li className="list-group-item"> An item</li>
+      <li className="list-group-item"> An item</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
